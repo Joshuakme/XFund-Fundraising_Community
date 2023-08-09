2945cfa942063ac9bd4d486baa790b6e995c2f86
Joshuakme <joshuakoh23@gmail.com>
2023-08-10 01:02:15 +0800

    Finalize Initial Commit

--- app/src/main/java/com/example/xfund/MainActivity.kt
+++ app/src/main/java/com/example/xfund/MainActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+//    fun writeNewUser(userId: String, name: String, email: String) {
+//        database = Firebase.database.reference
+//
+//        val user = User(name, email)
+//
+//        database.child("users").child(userId).setValue(user)
+//    }
 }
