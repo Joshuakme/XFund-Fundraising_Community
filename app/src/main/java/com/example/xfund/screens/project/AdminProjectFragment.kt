83ebace9fc196c70fcb0bf39a1fd84112b5cc2b2
NKL <klnick0523@gmail.com>
2023-09-30 17:16:16 +0800

    Edit and Delete function added

--- app/src/main/java/com/example/xfund/screens/project/AdminProjectFragment.kt
+++ app/src/main/java/com/example/xfund/screens/project/AdminProjectFragment.kt
@@ -1,23 +1,50 @@
 package com.example.xfund.screens.project
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.xfund.R
+import com.example.xfund.adapter.AdminProjectAdapter
 import com.example.xfund.databinding.FragmentAdminProjectBinding
+import com.example.xfund.model.Project
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import java.time.LocalDateTime
+import java.util.Calendar
+import java.util.Date
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AdminProjectFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AdminProjectFragment : Fragment() {
+class AdminProjectFragment : Fragment(),
+    AdminProjectAdapter.OnEditButtonClickListener,
+    AdminProjectAdapter.OnDeleteButtonClickListener
+{
     private lateinit var binding : FragmentAdminProjectBinding
+    private lateinit var projectAdminList : List<Project>
+
+    // Fetch data from Firebase
+    private val db = Firebase.firestore.collection("projects")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,13 +59,144 @@ class AdminProjectFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_admin_project, container, false)
 
-        binding.ProjectAddButton.setOnClickListener{
-            findNavController().navigate(R.id.action_adminProjectFragment_to_editProjectFragment)
-        }
+        val recyclerView: RecyclerView = binding.projectRecycleView
 
+        db.orderBy("name", Query.Direction.ASCENDING)
+            .get()
+            .addOnSuccessListener {
+                projectAdminList = createProject(it)
+
+                // RecyclerView
+                val adapter = AdminProjectAdapter(requireContext(), projectAdminList, this, this)
+                recyclerView.adapter = adapter
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(context, exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // ELEMENTS IN FRAGMENT
+        // Search Elements
+        val searchBar : SearchView = binding.SearchBar
+        val txtSearch = searchBar.findViewById(androidx.appcompat.R.id.search_src_text) as EditText
+        val searchIcon: ImageView = searchBar.findViewById(androidx.appcompat.R.id.search_mag_icon)
+        // Search Bar
+        searchIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.gray_300),android.graphics.PorterDuff.Mode.SRC_IN)
+        txtSearch.setHint(R.string.search_query_hint)
+        txtSearch.setHintTextColor(ContextCompat.getColor(requireContext(), R.color.gray_300))
+
+        binding.projectAddButton.setOnClickListener{
+            findNavController().navigate(R.id.action_adminProjectFragment_to_addProjectFragment)
+        }
 
         return binding.root
     }
 
+    private fun createProject(querySnapshot: QuerySnapshot): List<Project> {
+        val adminProjects = mutableListOf<Project>()
+
+        for (document in querySnapshot.documents) {
+            val cover = document.getString("cover") ?: ""
+            val name = document.getString("name") ?: ""
+            val description = document.getString("description") ?: ""
+            val startLocalDateTime = document.getDate("start_date")
+            val endLocalDateTime = document.getDate("end_date")
+            val fund_collected = document.getDouble("fund_collected") ?: 0.0
+            val fund_target = document.getDouble("fund_target") ?: 0.0
+
+            // Convert Date objects to LocalDateTime using Calendar
+            val startCalendar = Calendar.getInstance()
+            startCalendar.time = startLocalDateTime ?: Date(0) // Use some default date if start_date is null
+            val start_date = LocalDateTime.of(
+                startCalendar.get(Calendar.YEAR),
+                startCalendar.get(Calendar.MONTH) + 1,
+                startCalendar.get(Calendar.DAY_OF_MONTH),
+                startCalendar.get(Calendar.HOUR_OF_DAY),
+                startCalendar.get(Calendar.MINUTE)
+            )
+
+            val endCalendar = Calendar.getInstance()
+            endCalendar.time = endLocalDateTime ?: Date(0) // Use some default date if end_date is null
+            val end_date = LocalDateTime.of(
+                endCalendar.get(Calendar.YEAR),
+                endCalendar.get(Calendar.MONTH) + 1,
+                endCalendar.get(Calendar.DAY_OF_MONTH),
+                endCalendar.get(Calendar.HOUR_OF_DAY),
+                endCalendar.get(Calendar.MINUTE)
+            )
+
+            val adminProject = Project(cover, name, description, start_date, end_date, fund_collected, fund_target)
+            adminProjects.add(adminProject)
+        }
+
+        return adminProjects.toList()
+    }
+
+    override fun onEditClick(position: Int, project: Project) {
+        val action = AdminProjectFragmentDirections.actionAdminProjectFragmentToEditProjectFragment(project)
+        findNavController().navigate(action)
+    }
+
+    override fun onDeleteClick(position: Int, project: Project) {
+        val fragmentManager = requireActivity().supportFragmentManager
+        val dialog = DeleteButtonDialog()
+        val bundle = Bundle()
+        bundle.putParcelable("project", project)
+        dialog.arguments = bundle
+        dialog.show(fragmentManager, "DELETE_DIALOG")
+    }
+
+    inner class DeleteButtonDialog: DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            return activity?.let {
+
+                val project = arguments?.getParcelable<Project>("project")
+
+                // Use the Builder class for convenient dialog construction.
+                val builder = AlertDialog.Builder(it)
+                builder.setMessage("Do you want to delete this item?")
+                    .setPositiveButton("Delete") { dialog, id ->
+                        deleteProject(project)
+                        Toast.makeText(context, "Project Deleted", Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
+                    }
+                    .setNegativeButton("Cancel") { dialog, id ->
+                        Toast.makeText(context, "Deletion has been canceled", Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
+                    }
+                // Create the AlertDialog object and return it.
+                builder.create()
+            } ?: throw IllegalStateException("Activity cannot be null")
+        }
+
+        private fun deleteProject(oldProject: Project?) = CoroutineScope(
+            Dispatchers.IO).launch {
+            val projectQuery = db
+                .whereEqualTo("cover", oldProject?.cover)
+                .whereEqualTo("name", oldProject?.name)
+                .whereEqualTo("description", oldProject?.description)
+                .whereEqualTo("fund_target", oldProject?.fund_target)
+                .get()
+                .await()
+            if(projectQuery.documents.isNotEmpty()) {
+                for(document in projectQuery) {
+                    try {
+                        db.document(document.id)
+                            .delete()
+                            .addOnSuccessListener { Log.d("Edit Successful", "DocumentSnapshot successfully updated!") }
+                            .addOnFailureListener { e -> Log.w("Edit Failed", "Error updating document", e) }
+                    } catch(e: Exception) {
+                        Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(context, "No project matched the query.", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/xfund/screens/project/EditProjectFragment.kt
+++ app/src/main/java/com/example/xfund/screens/project/EditProjectFragment.kt
@@ -1,23 +1,41 @@
 package com.example.xfund.screens.project
 
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.example.xfund.R
 import com.example.xfund.databinding.FragmentEditProjectBinding
+import com.example.xfund.model.Project
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EditProjectFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditProjectFragment : Fragment() {
     private lateinit var binding: FragmentEditProjectBinding
+    private lateinit var updateButton: MaterialButton
+    private lateinit var projectNameText: TextInputEditText
+    private lateinit var projectDescText: TextInputEditText
+
+    private var isNameValid: Boolean = true
+    private var isDescValid: Boolean = true
+
+    // Connect to firestore projects collection
+    private val db = Firebase.firestore.collection("projects")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,15 +50,123 @@ class EditProjectFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,
         R.layout.fragment_edit_project, container, false)
 
+        //declare variables
+        updateButton = binding.EditProjectUpdateButton
+        projectNameText = binding.projectNameText
+        projectDescText = binding.projectDescriptionText
+
+
+        //set update button disabled as default
+        setBtnDisabled()
+
+        // Retrieve the project object from arguments
+        val project = arguments?.getParcelable<Project>("project")
+
+        // Access the properties of the Project object
+        val cover = project?.cover
+        val name = project?.name
+        val startDate = project?.start_date
+        val endDate = project?.end_date
+        val fundTarget = project?.fund_target
+        val fundCollected = project?.fund_collected
+        val description = project?.description
+
+        //Put the current project items into the TextInputEditText
+        projectNameText.text = Editable.Factory.getInstance().newEditable(name)
+        projectDescText.text = Editable.Factory.getInstance().newEditable(description)
+
+
+        // Event Listeners
+        projectNameText.addTextChangedListener {
+            val isNotEmpty = projectNameText.text?.isNotEmpty() == true //Check if there is at least 1 character
+            val isNotBlank = projectNameText.text?.isNotBlank() == true //Check if there is something inside except for empty spaces (space, tab)
+            val isLengthValid = projectNameText.length() >= 15
+
+            isNameValid = isNotEmpty && isNotBlank && isLengthValid
+
+            if(isFormInputValid()) {
+                setBtnEnabled()
+            } else {
+                setBtnDisabled()
+            }
+        }
+        projectDescText.addTextChangedListener {
+            val isNotEmpty = projectDescText.text?.isNotEmpty() == true //Check if there is at least 1 character
+            val isNotBlank = projectDescText.text?.isNotBlank() == true //Check if there is something inside except for empty spaces (space, tab)
+            val isLengthValid = projectDescText.length() >= 50
+
+            isDescValid = isNotEmpty && isNotBlank && isLengthValid
+
+            if(isFormInputValid()) {
+                setBtnEnabled()
+            } else {
+                setBtnDisabled()
+            }
+        }
+
+
+        //Navigation
+        binding.backButton.setOnClickListener{
+            findNavController().navigate(R.id.action_editProjectFragment_to_adminProjectFragment)
+        }
+
         binding.EditProjectCancelButton.setOnClickListener{
+            Toast.makeText(context, "Edit has been cancelled", Toast.LENGTH_LONG).show()
             findNavController().navigate(R.id.action_editProjectFragment_to_adminProjectFragment)
         }
 
         binding.EditProjectUpdateButton.setOnClickListener{
+            val editedProjectName = projectNameText.text.toString()
+            val editedProjectDesc = projectDescText.text.toString()
+            val edited = hashMapOf<String, Any>(
+                "name" to editedProjectName,
+                "description" to editedProjectDesc
+            )
+
+            editProject(project, edited)
             findNavController().navigate(R.id.action_editProjectFragment_to_adminProjectFragment)
         }
 
         return (binding.root)
+    }
+
+    private fun setBtnDisabled() {
+        updateButton.isEnabled = false
+        updateButton.setBackgroundColor(resources.getColor(R.color.button_disabled_bg))
+    }
+
+    private fun setBtnEnabled() {
+        updateButton.isEnabled = true
+        updateButton.setBackgroundColor(resources.getColor(R.color.accent_primary_300))
+    }
+
+    private fun editProject(oldProject: Project?, newProject: HashMap<String, Any>) = CoroutineScope(Dispatchers.IO).launch {
+        val projectQuery = db
+            .whereEqualTo("name", oldProject?.name)
+            .whereEqualTo("description", oldProject?.description)
+            .whereEqualTo("fund_target", oldProject?.fund_target)
+            .get()
+            .await()
+        if(projectQuery.documents.isNotEmpty()) {
+            for(document in projectQuery) {
+                try {
+                    db.document(document.id)
+                        .update(newProject)
+                        .addOnSuccessListener { Log.d("Edit Successful", "DocumentSnapshot successfully updated!") }
+                        .addOnFailureListener { e -> Log.w("Edit Failed", "Error updating document", e) }
+                } catch(e: Exception) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        } else {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, "No project matched the query.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun isFormInputValid(): Boolean {
+        return (isNameValid && isDescValid)
     }
 
 
