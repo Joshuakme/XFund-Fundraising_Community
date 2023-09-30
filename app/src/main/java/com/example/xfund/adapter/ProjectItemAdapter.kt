89484b74935f349c64e17129ea78793539426fc0
NKL <klnick0523@gmail.com>
2023-09-30 17:18:44 +0800

    Accessing firestore Cover picture working on ProjectsFragment

--- app/src/main/java/com/example/xfund/adapter/ProjectItemAdapter.kt
+++ app/src/main/java/com/example/xfund/adapter/ProjectItemAdapter.kt
@@ -1,22 +1,20 @@
 package com.example.xfund.adapter
 
 import android.content.Context
-
-import android.os.Build
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.xfund.databinding.ProjectsListCardCellBinding
 import com.example.xfund.model.Project
 import com.example.xfund.screens.navigation.ProjectsFragmentDirections
 
 class ProjectItemAdapter(
-    val context: Context, private val itemList: List<Project>, private val navController: NavController) :
-    RecyclerView.Adapter<ProjectItemAdapter.ViewHolder>() {
-    /*private var onClickListener: OnClickListener? = null*/
+    val context: Context,
+    private val itemList: List<Project>,
+    private val navController: NavController
+) : RecyclerView.Adapter<ProjectItemAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -28,32 +26,18 @@ class ProjectItemAdapter(
         return itemList.size
     }
 
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentItem = itemList[position]
         holder.bind(currentItem)
 
         holder.itemView.setOnClickListener {
-
             // Navigate to FragmentB when item is clicked
             val action = ProjectsFragmentDirections
                 .actionProjectsFragmentToProjectDetailFragment(
                     currentItem)
-            Log.d("ProjectItemAdapter", "Current item: $currentItem")
             navController.navigate(action)
         }
     }
-
-    /*// A function to bind the onclickListener.
-    fun setOnClickListener(onClickListener: OnClickListener) {
-        this.onClickListener = onClickListener
-    }
-
-    // onClickListener Interface
-    interface OnClickListener {
-        fun onClick(position: Int, model: Project)
-    }*/
 
     inner class ViewHolder(
         private val binding: ProjectsListCardCellBinding) :
@@ -63,6 +47,11 @@ class ProjectItemAdapter(
             binding.project = project
             // Ensure data binding updates immediately
             binding.executePendingBindings()
+
+            // Load image using Glide
+            Glide.with(binding.cardImage.context)
+                .load(project.cover)
+                .into(binding.cardImage)
         }
     }
 }
